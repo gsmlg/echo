88652682cf427180d93cb59693efebf9fde1498b
gsmlg <gsmlg@gsmlg.com>
2015-03-24 14:28:15 +0800

    add echo

---
+++ echo.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+server = http.createServer(function(req, res) {
+	var body = '';
+	req.on('data', function(trunk){
+		body += trunk;
+	})
+	console.log(Object.keys(req));
+	res.write(req.method + ' '+req.url + ' HTTP/' + req.httpVersion + '\r\n');
+	for (var i = 0, j = req.rawHeaders.length; i < j; i += 2) {
+		var head = req.rawHeaders;
+		res.write(head[i]+': '+head[i+1]+'\r\n');
+	}
+	res.write('\r\n');
+	req.on('end', function() {
+		res.end(body);
+	})
+});
+
+server.listen(10000, function () {
+	console.log('Server started at port: 10000!')
+});
