eb300dfa9443b2804bb5423056aa9b3a39700360
gsmlg <me@gsmlg.org>
2018-08-16 02:32:07 +0800

    add ssl and change entry

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@ FROM node:alpine
 
 MAINTAINER GSMLG < me@gsmlg.org >
 
-ENV NODE_PORT 80
-ENV NAME echo
+ENV NODE_PORT=80 \
+    NAME=echo \
+    SERVER_NAME=echo.gsmlg.org
 
 EXPOSE 80
 
-COPY server.js entrypoint.sh echo.conf /
+COPY server.js entrypoint.sh /
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,56 @@
 
 set -e
 
-NODE_PORT=${PORT:-80}
+NODE_PORT=${NODE_PORT:-80}
 
 export NODE_PORT
 
 [ ! -d /sites ] && mkdir /sites
 
-sed "s/SERVER_NAME/${NAME:-echo}/g" "/echo.conf" | \
-    sed "s/HOSTNAME/${HOSTNAME}/g" > "/sites/echo"
+NAME=${NAME:-echo}
+SERVER_NAME=${SERVER_NAME:-echo.gsmlg.org}
+
+cat <<EOF > /sites/echo
+upstream @echo {
+  server $NAME:$NODE_PORT;
+}
+
+server {
+  listen 80;
+  listen [::]:80;
+  server_name $SERVER_NAME;
+
+  access_log /var/log/nginx/echo.access.log;
+  error_log /var/log/nginx/echo.error.log;
+
+  location / {
+    add_header Access-Control-Allow-Origin *;
+    add_header Access-Remote-Addr \$remote_addr;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header host \$host;
+    proxy_pass http://@echo;
+  }
+}
+
+server {
+  listen [::]:443 ssl http2;
+  listen 443 ssl http2;
+  server_name $SERVER_NAME;
+
+  access_log /var/log/nginx/echo.access.log;
+  error_log /var/log/nginx/echo.error.log;
+
+  include /etc/nginx/ssl.conf;
+
+  location / {
+    add_header Access-Control-Allow-Origin *;
+    add_header Access-Remote-Addr \$remote_addr;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header host \$host;
+    proxy_pass http://@echo;
+  }
+}
+
+EOF
 
 "node" "/server.js"
-
