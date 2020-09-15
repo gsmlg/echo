7d8f9307447803b645277945ab100d97a18cab5a
gsmlg <me@gsmlg.org>
2020-09-15 11:19:13 +0800

    refact echo

--- echo.go
+++ echo.go
@@ -1,56 +1,51 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "io"
-    "io/ioutil"
-    "time"
-    "fmt"
-    "os"
+        "net/http"
+        "log"
+        "io"
+        "time"
+        "fmt"
+        "os"
 )
 
 func myHandler (w http.ResponseWriter, r *http.Request) {
-    str := fmt.Sprintf("====> New request from %s \r\n", r.RemoteAddr)
-    os.Stdout.WriteString(str)
+        str := fmt.Sprintf("====> New request from %s \r\n", r.RemoteAddr)
+        os.Stdout.WriteString(str)
 
-    str = fmt.Sprintf("%s %s\r\n", r.Method, r.URL)
-    io.WriteString(w, str)
-    io.WriteString(os.Stdout, str)
-    for k,v := range(r.Header) {
-        for _, vv := range(v) {
-            str := fmt.Sprintf("%s: %s\r\n", k, vv)
-            io.WriteString(w, str)
-            io.WriteString(os.Stdout, str)
+        echo := io.MultiWriter(w, os.Stdout)
+        str = fmt.Sprintf("%s %s %s\r\n", r.Method, r.URL, r.Proto)
+        io.WriteString(echo, str)
+
+        for k,v := range(r.Header) {
+                for _, vv := range(v) {
+                        str := fmt.Sprintf("%s: %s\r\n", k, vv)
+                        io.WriteString(echo, str)
+                }
         }
-    }
-    io.WriteString(w, "\r\n")
-    io.WriteString(os.Stdout, "\r\n")
+        io.WriteString(echo, "\r\n")
 
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        log.Printf("Error reading body: %v", err)
-        http.Error(w, "can't read body", http.StatusBadRequest)
-        return
-    }
-    w.Write(body)
-    os.Stdout.Write(body)
+        _, err := io.Copy(echo, r.Body)
+        if err != nil {
+                log.Printf("Error reading body: %v", err)
+                http.Error(w, "can't read body", http.StatusBadRequest)
+                os.Stdout.WriteString("\r\n====> End request With Error \r\n")
+                return
+        }
 
-    io.WriteString(w, "\r\n")
-    io.WriteString(os.Stdout, "\r\n")
+        os.Stdout.WriteString("\r\n====> End request \r\n")
 }
 
 func main() {
 
-    s := &http.Server{
-        Addr:           ":80",
-        Handler:        http.HandlerFunc(myHandler),
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
+        s := &http.Server{
+                Addr:           ":8088",
+                Handler:        http.HandlerFunc(myHandler),
+                ReadTimeout:    10 * time.Second,
+                WriteTimeout:   10 * time.Second,
+                MaxHeaderBytes: 1 << 20,
+        }
 
-    log.Fatal(s.ListenAndServe())
+        log.Fatal(s.ListenAndServe())
 
 }
-
